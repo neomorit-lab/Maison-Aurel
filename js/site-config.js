210bf4b9edb64bcd62e596d0a5b4f599e71d0402
yassine <yassine@mail.com>
2026-08-01 12:40:15 +0100

    Set WhatsApp contact number

--- js/site-config.js
+++ js/site-config.js
@@ -1,5 +1,5 @@
 window.MAISON_AUREL_CONFIG = {
-  whatsappNumber: "",
+  whatsappNumber: "212652563924",
   whatsappDefaultMessage: "Bonjour Maison Aurel, je souhaite un conseil pour une pierre ou une création en or.",
   city: "Rabat",
   country: "MA"
